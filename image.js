c9d4fbd9155b8384f99826dca8731b1c8540eab8
Nocturnalx <joedavison@btinternet.com>
2025-04-20 16:50:51 +0100

    first working version

--- image.js
+++ image.js
@@ -1,36 +1,16 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+require('dotenv').config();
+
+//make sure the folders in folders.json exist - create if not
+const {checkFolders} = require('./lib/folders');
+const folders = process.env.FOLDERS.split(',');
+
+checkFolders(folders);
 
 
-const data = fs.readFileSync('folders.json', 'utf-8');
-const folders = JSON.parse(data);
+//create express app and set port to listen
+const app = require('./lib/exp');
+const PORT = 3004;
 
-for (const folder of folders) {
-	const folderPath = path.resolve(folder.path);
-	if (!fs.existsSync(folderPath) || !fs.lstatSync(folderPath).isDirectory()) {
-		throw new Error(`Folder "${folder.name}" does NOT exist at ${folderPath}`);
-	}
-	console.log(`Folder "${folder.name}" exists at ${folderPath}`);
-}
-
-
-const app = express();
-
-const upload = (req, res) => {
-    // if !req.headers['x-filename'] throw ;
-
-    const filename = req.headers['x-filename'];
-	const filepath = path.join(__dirname, 'uploads', filename);
-
-	const writeStream = fs.createWriteStream(filepath);
-	req.pipe(writeStream);
-
-	req.on('end', () => res.send('Image received and saved'));
-	req.on('error', err => {
-		console.error('Upload error:', err);
-		res.status(500).send('Failed to save image');
-	});
-}
-
-app.post('upload/:folder', upload)
+app.listen(PORT, ()=>{
+	console.log(`image server listening on ${PORT}`);
+});
